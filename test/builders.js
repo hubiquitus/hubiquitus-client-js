5b54bd2f1badd8ed6693736eecefd2181782c7ee
jaimesDSG <jaimes.dsg@gmail.com>
2012-06-15 10:46:51 +0200

    added test cases for builders and unsubscribe

---
+++ test/builders.js
@@ -0,0 +1,251 @@
+/*
+ * Copyright (c) Novedia Group 2012.
+ *
+ *     This file is part of Hubiquitus.
+ *
+ *     Hubiquitus is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Hubiquitus is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Hubiquitus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var should = require("should");
+var hClient = require('../hubiquitus.js').hClient;
+var conf = require('./testConfig.js');
+/*
+ Because the throws are in another file, the condition should.throw() does not work.
+ Using instead: try-catch + should in the caught error
+ */
+describe('#buildMessage()', function() {
+
+    var chid = 'chan';
+
+    it('should throw an error if chid not provided', function(done) {
+        try {
+            hClient.buildMessage();
+        } catch (error) {
+            should.exist(error.message);
+            done();
+        }
+    });
+
+    it('should create a message if chid provided', function(done) {
+        try {
+            hClient.buildMessage(chid);
+            done();
+        } catch (error) {
+        }
+    });
+
+
+
+})
+
+describe('#buildMeasure()', function() {
+
+    var chid = 'chan';
+    var value = 10;
+    var unit = 'meter';
+
+    it('should throw an error if nothing provided', function(done) {
+        try {
+            hClient.buildMeasure();
+        } catch (error) {
+            should.exist(error.message);
+            done();
+        }
+    });
+
+    it('should throw an error if value not provided but chid provided', function(done) {
+        try {
+            hClient.buildMeasure(chid);
+        } catch (error) {
+            should.exist(error.message);
+            done();
+        }
+    });
+
+    it('should throw an error if unit not provided but chid and value provided', function(done) {
+        try {
+            hClient.buildMeasure(chid,value);
+        } catch (error) {
+            should.exist(error.message);
+            done();
+        }
+    });
+
+    it('should throw an error if chid not provided but value and unit provided', function(done) {
+        try {
+            hClient.buildMeasure(undefined,value,unit);
+        } catch (error) {
+            should.exist(error.message);
+            done();
+        }
+    });
+
+    it('should throw an error if unit not provided but chid and unit provided', function(done) {
+        try {
+            hClient.buildMeasure(chid,undefined,unit);
+        } catch (error) {
+            should.exist(error.message);
+            done();
+        }
+    });
+
+    it('should throw an error if unitis only provided', function(done) {
+        try {
+            hClient.buildMeasure(undefined,undefined,unit);
+        } catch (error) {
+            should.exist(error.message);
+            done();
+        }
+    });
+
+    it('should create a measure if all provided', function(done) {
+        try {
+            hClient.buildMessage(chid,value,unit);
+            done();
+        } catch (error) {
+        }
+    });
+
+})
+
+describe('#buildAck()', function() {
+
+    var chid = 'chan';
+    var ackid = 'ackid';
+    var ack = 'recv';
+    var options = {convid : "convid"};
+
+    it('should throw an error if nothing provided', function(done) {
+        try {
+            hClient.buildAck();
+        } catch (error) {
+            should.exist(error.message);
+            done();
+        }
+    });
+
+    it('should throw an error if ackid not provided but chid provided', function(done) {
+        try {
+            hClient.buildAck(chid);
+        } catch (error) {
+            should.exist(error.message);
+            done();
+        }
+    });
+
+    it('should throw an error if ack not provided but chid, ackid provided', function(done) {
+        try {
+            hClient.buildAck(chid,ackid);
+        } catch (error) {
+            should.exist(error.message);
+            done();
+        }
+    });
+
+    it('should throw an error if options not provided but chid, ackid and ack provided', function(done) {
+        try {
+            hClient.buildAck(chid,ackid,ack);
+        } catch (error) {
+            should.exist(error.message);
+            done();
+        }
+    });
+
+    it('should create an ack if all provided', function(done) {
+        try {
+            hClient.buildAck(chid,ackid,ack,options);
+            done();
+        } catch (error) {
+        }
+    });
+
+})
+
+describe('#buildConv()', function() {
+
+    var chid = 'chan';
+    var topic = 'topic';
+    var participants = [];
+
+    it('should throw an error if nothing provided', function(done) {
+        try {
+            hClient.buildConv();
+        } catch (error) {
+            should.exist(error.message);
+            done();
+        }
+    });
+
+    it('should throw an error if topic not provided but chid provided', function(done) {
+        try {
+            hClient.buildConv(chid);
+        } catch (error) {
+            should.exist(error.message);
+            done();
+        }
+    });
+
+    it('should throw an error if participants not provided but chid, topic provided', function(done) {
+        try {
+            hClient.buildConv(chid,topic);
+        } catch (error) {
+            should.exist(error.message);
+            done();
+        }
+    });
+
+    it('should create an ack if all provided', function(done) {
+        try {
+            hClient.buildConv(chid,topic,participants);
+            done();
+        } catch (error) {
+        }
+    });
+
+})
+
+
+describe('#buildAlert()', function() {
+
+    var chid = 'chan';
+    var alert = 'alert';
+
+    it('should throw an error if nothing provided', function(done) {
+        try {
+            hClient.buildAlert();
+        } catch (error) {
+            should.exist(error.message);
+            done();
+        }
+    });
+
+    it('should throw an error if alert not provided but chid provided', function(done) {
+        try {
+            hClient.buildAlert(chid);
+        } catch (error) {
+            should.exist(error.message);
+            done();
+        }
+    });
+
+    it('should create an ack if all provided', function(done) {
+        try {
+            hClient.buildAlert(chid,alert);
+            done();
+        } catch (error) {
+        }
+    });
+
+})
+
---
+++ test/unsubscribe.js
@@ -0,0 +1,150 @@
+/*
+ * Copyright (c) Novedia Group 2012.
+ *
+ *     This file is part of Hubiquitus.
+ *
+ *     Hubiquitus is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Hubiquitus is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Hubiquitus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var should = require("should");
+var hClient = require('../hubiquitus.js').hClient;
+var conf = require('./testConfig.js');
+
+describe('#unsubscribe()', function() {
+
+    var user = conf.logins[0];
+    var chanActive = 'chan' + Math.floor(Math.random()*10000);
+    var chanInactive = 'chan' + Math.floor(Math.random()*10000);
+    var chanInactiveNotSubscribed = 'chan' + Math.floor(Math.random()*10000);
+
+    before(conf.connect)
+
+    after(conf.disconnect)
+
+    before(function(done){
+        conf.createChannel(chanActive, user.login, [user.login], true, done);
+    })
+
+    before(function(done){
+        conf.createChannel(chanInactive, user.login, [user.login], true, done);
+    })
+
+    before(function(done){
+        conf.createChannel(chanInactiveNotSubscribed, user.login, [conf.logins[1]], false, done);
+    })
+
+    before(function(done){
+        hClient.subscribe(chanActive, function(hResult){
+            done();
+        });
+    })
+
+    before(function(done){
+        hClient.subscribe(chanInactive, function(hResult){
+            done();
+        });
+    })
+
+    before(function(done){
+        conf.createChannel(chanInactive, user.login, [user.login], false, done);
+    });
+
+    it('should return hResult status NOT_AUTHORIZED and result be a message if channel does not exist', function(done) {
+        hClient.unsubscribe("This chan does not exist", function(hResult){
+            hResult.status.should.be.eql(hClient.hResultStatus.NOT_AUTHORIZED);
+            hResult.result.should.be.a('string');
+            done();
+        });
+    })
+
+    it('should return hResult status NOT_AUTHORIZED and result be a message if channel inactive and user was subscribed', function(done) {
+        hClient.unsubscribe(chanInactive, function(hResult){
+            hResult.status.should.be.eql(hClient.hResultStatus.NOT_AUTHORIZED);
+            hResult.result.should.be.a('string');
+            done();
+        });
+    })
+
+    it('should return hResult status NOT_AUTHORIZED and result be a message if channel inactive and user not subscribed', function(done) {
+        hClient.unsubscribe(chanInactiveNotSubscribed, function(hResult){
+            hResult.status.should.be.eql(hClient.hResultStatus.NOT_AUTHORIZED);
+            hResult.result.should.be.a('string');
+            done();
+        });
+    })
+
+    it('should return hResult status MISSING_ATTR and result be a message if chid not provided', function(done) {
+        hClient.unsubscribe(undefined, function(hResult){
+            hResult.status.should.be.eql(hClient.hResultStatus.MISSING_ATTR);
+            hResult.result.should.be.a('string');
+            done();
+        });
+    })
+
+    it('should return hResult status OK if subscribed and in participants list', function(done) {
+        hClient.unsubscribe(chanActive, function(hResult){
+            hResult.status.should.be.eql(hClient.hResultStatus.OK);
+            done();
+        });
+    })
+
+    it('should return hResult status NOT_AUTHORIZED and result be a message if user not subscribed', function(done) {
+        hClient.unsubscribe(chanActive, function(hResult){
+            hResult.status.should.be.eql(hClient.hResultStatus.NOT_AUTHORIZED);
+            hResult.result.should.be.a('string');
+            done();
+        });
+    })
+
+})
+
+describe('#unsubscribe()', function() {
+
+    var user = conf.logins[0];
+    var chanActive = 'chan' + Math.floor(Math.random()*10000);
+
+    it('should return hResult status NOT_CONNECTED and result be a message if user not connected', function(done) {
+        hClient.unsubscribe(chanActive, function(hResult){
+            hResult.status.should.be.eql(hClient.hResultStatus.NOT_CONNECTED);
+            hResult.result.should.be.a('string');
+            done();
+        });
+    })
+
+})
+
+describe('#unsubscribe()', function() {
+
+    var user = conf.logins[0];
+    var chanActive = 'chan' + Math.floor(Math.random()*10000);
+
+    before(conf.connect)
+
+    after(conf.disconnect)
+
+    before(function(done){
+        conf.createChannel(chanActive, user.login, [user.login], true, done);
+    })
+
+    it('should return hResult status NOT_AUTHORIZED and result be a message if user not subscribed', function(done) {
+        hClient.unsubscribe(chanActive, function(hResult){
+            hResult.status.should.be.eql(hClient.hResultStatus.NOT_AUTHORIZED);
+            hResult.result.should.be.a('string');
+            done();
+        });
+    })
+
+})
+
+
