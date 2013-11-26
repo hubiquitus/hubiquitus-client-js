b859bc52e0c4880e676d8168fbb60f94106b421c
etissieres <etienne.tissieres@gmail.com>
2013-11-26 17:29:17 +0100

    bug fixes & minor updates

--- hubiquitus.js
+++ hubiquitus.js
@@ -1,136 +1,136 @@
-define(['lodash', 'sockjs', 'util', 'events', 'logger'], function (_, sock, util, EventEmitter, logger) {
+define(['lodash', 'sockjs', 'util', 'events', 'logger'], function (_, SockJS, util, EventEmitter, logger) {
   'use strict';
 
   var defaultSendTimeout = 30000;
   var maxSendTimeout = 5 * 3600000;
 
-  return window.Hubiquitus = (function() {
+  var exports = window.Hubiquitus = (function() {
 
     function Hubiquitus() {
       EventEmitter.call(this);
-      this._locked = false;
       this._sock = null;
+      this._events = new EventEmitter();
       this.id = null;
     }
 
     util.inherits(Hubiquitus, EventEmitter);
 
     Hubiquitus.prototype.connect = function (endpoint, authData, cb) {
-      logger.info('connect');
+      if (this._sock) {
+        logger.warn('hubiquitus is busy, cant connect ' + endpoint);
+        return this;
+      }
+
+      var _this = this;
+
+      cb && this.once('connect', cb);
+      this._sock = new SockJS(endpoint);
+
+      this._sock.onopen = function () {
+        logger.info('hubiquitus connected');
+        var message = encode({type: 'login', authData: authData});
+        message && _this._sock.send(message);
+      };
+
+      this._sock.onclose = function () {
+        logger.info('hubiquitus disconnected');
+        _this._sock = null;
+        _this.emit('disconnect');
+      };
+
+      this._sock.onmessage = function (e) {
+        logger.trace('hubiquitus received message', e.data);
+        var message = decode(e.data);
+        if (!message) return;
+        switch (message.type) {
+          case 'message':
+            _this._onMessage(message);
+            break;
+          case 'response':
+            _this._events.emit('response|' + message.id, message);
+            break;
+          case 'login':
+            logger.info('Hubiquitus logged in; identifier is', message.payload.content.id);
+            _this.id = message.payload.content.id;
+            _this.emit('connect');
+            break;
+          default:
+            logger.warn('Hubiquitus received unknown message type', message);
+        }
+      };
+
+      return this;
     };
 
     Hubiquitus.prototype.disconnect = function () {
-      logger.info('disconnect');
+      if (!this._sock) {
+        logger.warn('hubiquitus is already idle');
+        return this;
+      }
+
+      this._sock && this._sock.close();
+      return this;
     };
 
     Hubiquitus.prototype.send = function (to, content, timeout, cb) {
-      logger.info('send');
+      var _this = this;
+
+      if (_.isFunction(timeout)) { cb = timeout; timeout = defaultSendTimeout; }
+      timeout = timeout ||  maxSendTimeout;
+      var message = {to: to, payload: {content: content}, id: util.uuid(), date: (new Date()).getTime(), type: 'message'};
+
+      _this._events.once('response|' + message.id, function (response) {
+        _this._onResponse(response, cb);
+      });
+
+      setTimeout(function () {
+        _this._events.emit('response|' + message.id, {payload: {err: 'TIMEOUT'}});
+      }, timeout);
+
+      logger.trace('hubiquitus sending message', message);
+      message = encode(message);
+      message && this._sock.send(message);
+
+      return this;
     };
+
+    Hubiquitus.prototype._onMessage = function (message) {
+      logger.trace('Hubiquitus processing message', message);
+      this.emit('message', message.from, message.payload.content, function (err, content) {
+        var response = {to: message.from, id: message.id, payload: {err: err, content: content}, type: 'response'};
+        response = encode(response);
+        response && this._sock.send(response);
+      });
+    };
+
+    Hubiquitus.prototype._onResponse = function (response, cb) {
+      logger.trace('Hubiquitus processing response', response);
+      cb && cb(response.payload.err, response.payload.content);
+    };
+
+    function encode(data) {
+      var encodedData = null;
+      try {
+        encodedData = JSON.stringify(data);
+      } catch (err) {
+        logger.warn('hubiquitus failed encoding data', data);
+      }
+      return encodedData;
+    }
+
+    function decode(data) {
+      var decodedData = null;
+      try {
+        decodedData = JSON.parse(data);
+      } catch (err) {
+        logger.warn('hubiquitus failed decoding data', data);
+      }
+      return decodedData;
+    }
 
     return Hubiquitus;
   })();
 
-
-  // TMP DISABLED
-
-
-  var sock = null;
-  var exports = {};
-
-  exports.id = null;
-
-  /**
-   * Connect a hubiquitus container
-   * @param addr {string} connection addr
-   */
-  exports.connect = function (addr, authData) {
-    if (sock) exports.disconnect();
-    sock = new SockJS(addr);
-
-    sock.onopen = function () {
-      console.log('Hubiquitus connected to ' + addr);
-      events.emit('hubiquitus started');
-      if (!exports.id) {
-        var message = {authData: authData, type: 'login'};
-        console.log('Hubiquitus login ', message);
-        sock.send(JSON.stringify(message));
-      }
-    };
-
-    sock.onclose = function () {
-      console.log('Hubiquitus disconnected from ' + addr);
-      events.emit('hubiquitus stopped');
-    };
-
-    sock.onmessage = function (e) {
-      console.log('Hubiquitus received a message from ' + addr);
-      onMessageInternal(e.data);
-    };
-  };
-
-  /**
-   * Disconnect from hubiquitus container
-   */
-  exports.disconnect = function () {
-    sock && sock.close();
-    sock = null;
-  };
-
-  /**
-   * Sends a message to a foreign actor actor
-   * @param to {string} target actor id
-   * @param content {*} message content
-   * @param [timeout] {number} message feedback timeout
-   * @param [cb] {function} message feedback
-   */
-  exports.send = function (to, content, timeout, cb) {
-    if (_.isFunction(timeout)) { cb = timeout; timeout = defaultSendTimeout; }
-    timeout = timeout ||  maxSendTimeout;
-
-    var message = {to: to, payload: {content: content}, id: uuid(), date: (new Date()).getTime(), type: 'message'};
-
-    events.once('response|' + message.id, function (err, response) {
-      return err ? cb(err) : cb(response.payload.err, response.payload.content);
-    });
-
-    setTimeout(function () {
-      events.emit('response|' + message.id, 'TIMEOUT');
-    }, timeout);
-
-    console.log('Hubiquitus sending ', message);
-    sock.send(JSON.stringify(message));
-  };
-
-  /**
-   * Internal message handler
-   * @param data {string} incomming json data
-   */
-  function onMessageInternal (data) {
-    var message;
-    try {
-      message = JSON.parse(data);
-    } catch (err) {
-      return console.log('Hubiquitus failed to parse incomming message', data);
-    }
-    switch (message.type) {
-      case ' ':
-        console.log('Hubiquitus processing message', message);
-        exports.onMessage(message.from, message.payload.content, function (err, content) {
-          var response = {to: message.from, id: message.id, payload: {err: err, content: content}, type: 'response'};
-          sock.send(response);
-        });
-        break;
-      case 'response':
-        console.log('Hubiquitus processing response', message);
-        events.emit('response|' + message.id, null, message);
-        break;
-      case 'login':
-        console.log('Hubiquitus login feedback', message);
-        exports.id = message.aid;
-        events.emit('login', exports.id);
-        break;
-      default:
-        console.log('Hubiquitus received unknown message type', message);
-    }
-  }
+  exports.logger = logger;
+  return exports;
 });
