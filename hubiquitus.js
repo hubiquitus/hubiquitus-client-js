1a167f1caf4296eb7ce44233e27b68d018b45f76
Nadim <nadim.boustani@gmail.com>
2013-12-03 23:18:53 +0100

    Bug fix onMessage arguments

--- hubiquitus.js
+++ hubiquitus.js
@@ -101,7 +101,7 @@ define(['lodash', 'sockjs', 'util', 'events', 'logger'], function (_, SockJS, ut
     Hubiquitus.prototype._onReq = function (req) {
       logger.trace('processing message', req);
       var _this = this;
-      this.emit('message', req.from, req.content, function (err, content) {
+      this.emit('message', req, function (err, content) {
         var res = {to: req.from, id: req.id, err: err, content: content, type: 'res'};
         res = encode(res);
         res && _this._sock.send(res);
