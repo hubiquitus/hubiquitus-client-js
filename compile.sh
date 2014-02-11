da23fc19ff511794e655ab3ce263b1e64592cc26
etissieres <etienne.tissieres@gmail.com>
2014-02-11 18:16:46 +0100

    remove require dependency

---
+++ compile.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+uglify=./node_modules/uglify-js/bin/uglifyjs
+if [ ! -f $uglify ]
+then
+  echo "> ERROR : $uglify  not found in node modules !"
+  echo "did you run npm install ?"
+  exit -1
+fi
+
+lib="hubiquitus.js"
+minlib="hubiquitus.min.js"
+
+echo "> cleaning previous build..."
+rm $lib $minlib
+
+echo "> building $lib..."
+touch $lib
+parts=(
+  "vendor/sockjs.js"
+  "vendor/lodash.js"
+  "lib/util.js"
+  "lib/logger.js"
+  "lib/events.js"
+  "lib/transport.js"
+  "lib/hubiquitus.js"
+)
+for part in "${parts[@]}"
+do
+  echo "merging $part in $lib..."
+  cat $part >> $lib
+  echo -e >> $lib
+done
+
+echo "> building $minlib..."
+uglifyjs $lib -o $minlib
